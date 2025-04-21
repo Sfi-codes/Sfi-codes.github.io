dc0bd584df51c785e396d74876520a0013f78746
Sfi-codes <sfi0220@protonmail.com>
2025-04-21 18:24:14 +0100

    fix for clash project link

--- scripts/index.js
+++ scripts/index.js
@@ -1,59 +1,59 @@
-const projects = [{
+const projects = [
+  {
     projectLink: `project-sfi-codes.html`,
-    thumbnail: 'SfiCodes',
-    projectText: 'Personal Website'
-},
-{
+    thumbnail: "SfiCodes",
+    projectText: "Personal Website",
+  },
+  {
     projectLink: `project-plant-trees.html`,
-    thumbnail: 'PlantTrees',
-    projectText: 'Kotlin Mobile App'
-},
-{
+    thumbnail: "PlantTrees",
+    projectText: "Kotlin Mobile App",
+  },
+  {
     projectLink: `project-clash.html`,
-    thumbnail: 'Clash',
-    projectText: 'Java Secure Chat'
-},
-{
+    thumbnail: "Clash",
+    projectText: "C# Multiplayer Game",
+  },
+  {
     projectLink: `project-event-website.html`,
-    thumbnail: 'Events Website',
-    projectText: 'PHP Website'
-},
-{
+    thumbnail: "Events Website",
+    projectText: "PHP Website",
+  },
+  {
     projectLink: `project-task-management.html`,
-    thumbnail: 'Task Management App',
-    projectText: 'MERN Application'
-},
-]
+    thumbnail: "Task Management",
+    projectText: "MERN Application",
+  },
+];
 
 const displayProjects = () => {
+  const projectsContainerDiv = document.createElement("div");
+  projectsContainerDiv.className = "project-container";
+  {
+    projects.map((project) => {
+      const thumbnailDiv = document.createElement("div");
+      const textDiv = document.createElement("div");
 
-    const projectsContainerDiv = document.createElement('div')
-    projectsContainerDiv.className = 'project-container'
-    {
-        projects.map(project => {
-            const thumbnailDiv = document.createElement('div')
-            const textDiv = document.createElement('div')
+      const linkElement = document.createElement("a");
+      linkElement.href = project.projectLink;
 
-            const linkElement = document.createElement('a');
-            linkElement.href = project.projectLink
+      thumbnailDiv.className = "project-thumbnail";
+      thumbnailDiv.textContent = project.thumbnail;
 
-            thumbnailDiv.className = 'project-thumbnail'
-            thumbnailDiv.textContent = project.thumbnail
+      textDiv.className = "project-text";
+      textDiv.textContent = project.projectText;
 
-            textDiv.className = 'project-text'
-            textDiv.textContent = project.projectText
+      linkElement.appendChild(thumbnailDiv);
+      linkElement.appendChild(textDiv);
 
-            linkElement.appendChild(thumbnailDiv)
-            linkElement.appendChild(textDiv)
+      const projectContainer = document.createElement("div");
+      projectContainer.appendChild(linkElement);
 
-            const projectContainer = document.createElement('div')
-            projectContainer.appendChild(linkElement)
+      projectsContainerDiv.appendChild(projectContainer);
+    });
+  }
+  return projectsContainerDiv;
+};
 
-            projectsContainerDiv.appendChild(projectContainer)
-        })
-    }
-    return projectsContainerDiv
-}
-
-const app = document.getElementById('projects')
-app.appendChild(displayProjects())
+const app = document.getElementById("projects");
+app.appendChild(displayProjects());
